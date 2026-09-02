2389d928d0a28575f717683c4aa0b9bc158ef19e
Shreya Shinde <shindeshreya632@gmail.com>
2026-09-02 22:18:07 +0530

    Update UBNC7.C

--- UBNC7.C
+++ UBNC7.C
@@ -1,7 +1,8 @@
 /*
 Program (7) -> Write a C program to swap two
 numbers using a temporary variable.
-Solution (2) -> User Defined Values
+
+Solution (1) -> Predefined Values
 */
 
 #include <stdio.h>
@@ -9,20 +10,19 @@ Solution (2) -> User Defined Values
 int main()
 {
     int a, b, temp;
-a=111;
-b=222;
 
-    printf("\n Beforeswapping:");
+    a = 111;
+    b = 222;
 
-    printf("\n a=%d,b = %d\n",a, b);
-    
+    printf("\nBefore swapping:");
+    printf("\na = %d, b = %d\n", a, b);
+
     temp = a;
     a = b;
     b = temp;
 
     printf("\nAfter swapping:");
-    printf("\n a=%d,b = %d\n",a, b);
-    
+    printf("\na = %d, b = %d\n", a, b);
 
     return 0;
 }
