ae38f44044eb44e2cfb26415fb76c4eadf205d7d
Shreya Shinde <shindeshreya632@gmail.com>
2026-09-02 21:37:18 +0530

    Create UBNC1.c

---
+++ UBNC1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum = 55;
+    per = 88.88;
+    grade = 'A';
+
+    printf("----- Student Information -----\n");
+    printf("\nRoll Number : %d", rollnum);
+    printf("\nPercentage : %f", per);
+    printf("\nGrade : %c", grade);
+
+    return 0;
+}
