a8d69fc69002ddf9d75148ba79d42b2bf36387b3
Shreya Shinde <shindeshreya632@gmail.com>
2026-09-02 21:44:18 +0530

    Create UBNC3.C

---
+++ UBNC3.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius = 1.1;
+    float area;
+
+    area = (3.14 * radius * radius);
+
+    printf("\nArea of circle = %f", area);
+
+    return 0;
+}
